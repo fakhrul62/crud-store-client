c19c9ec34c5b26101c00af5abcbe41225121f744
fakhrul62 <ifakhrul23@gmail.com>
2024-12-07 04:47:54 +0600

    firebase env local added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyBw_jyMo8_5o8QTpqvFRR24-EaFBk-y4N4",
-  authDomain: "crud-store-5e285.firebaseapp.com",
-  projectId: "crud-store-5e285",
-  storageBucket: "crud-store-5e285.firebasestorage.app",
-  messagingSenderId: "592950908862",
-  appId: "1:592950908862:web:391db2d4e278075d1f0bef"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
